e63de180f6eeaecd13ac30a54c44cee0a0d6c73c
mhelvens <mhelvens@gmail.com>
2015-04-03 17:00:35 +0200

    dummy change to push for Travis build

--- karma.ci.conf.js
+++ karma.ci.conf.js
@@ -1,5 +1,5 @@
 module.exports = function (config) {
 	var conf = require('./karma.common.js');
 	conf.reporters.push('coveralls'); // needed for coverage results online
-	config.set(conf)
+	config.set(conf);
 };
--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,5 @@
 module.exports = function (config) {
 	var conf = require('./karma.common.js');
 	conf.preprocessors['./src/*.es6.js'] = ['coverage']; // needed for coverage results in WebStorm
-	config.set(conf)
+	config.set(conf);
 };
